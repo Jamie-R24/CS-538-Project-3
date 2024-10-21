ff912cf9d88368b78be47e6726f3d8dc19903c0a
Jamie-R24 <jamierapal@gmail.com>
2024-10-21 14:00:04 -0400

    Reverted. 2 EDAS Done

--- Sander_Coscia_Jamie_Rapal.py
+++ Sander_Coscia_Jamie_Rapal.py
@@ -5,13 +5,6 @@ from collections import defaultdict
 from PIL import Image
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn.svm import SVR
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import classification_report
 
 images = 'crop_part1'
 images_array = []
@@ -22,22 +15,18 @@ for filename in os.listdir(images):
     if filename.endswith('.jpg'):
         age = int(filename.split('_')[0])
         img_path = os.path.join(images, filename)
-        img = Image.open(img_path)
-
+        img = Image.open(img_path).resize((224,224)).convert('RGB')
         img_sizes[img.size] += 1
-
-        img = img.convert('L')
-        img_array = np.array(img).flatten()
-        images_array.append(img_array)
-        labels.append(age)
-
         
+        img_array = np.array(img)      
+        
+        labels.append(age)        
 
 #--------------------------------------------------------------
 #---------------------Task 2: EDA------------------------------
 #--------------------------------------------------------------
 
-#Evaluating Distribution of Ages in data
+# #Evaluating Distribution of Ages in data
 df = pd.DataFrame(labels, columns=['Age'])
 
 plt.figure(figsize=(12,6))
@@ -68,5 +57,5 @@ plt.ylabel('Count')
 
 
 
-#Shows all plots
+# #Shows all plots
 plt.show()
